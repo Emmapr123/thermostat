6e9ee10aadddf226d2c393ca1dd237651c81edf2
Emmapr123 <emmapriester@hotmail.com>
2021-04-07 16:36:48 +0100

    temp can go up and down in steps of 1

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,6 +1,22 @@
 describe('thermostat', function() {
   let thermostat = new Thermostat;
+
   it ('starts at 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20)
   });
+
+  describe('up', function() {
+    it('increases the temperature', function() {
+      thermostat.up();
+      expect(thermostat.temperature).toEqual(21)
+    });
+  });
+
+  describe('down', function() {
+    it ('decreases the temperature by 1', function() {
+      thermostat.down()
+      thermostat.down()
+      expect(thermostat.temperature).toEqual(19)
+    })
+  })
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,5 +1,12 @@
 class Thermostat {
   constructor(temperature) {
     this.temperature = 20;
-  }
+  };
+  up() {
+    this.temperature++;
+  };
+
+  down() {
+    this.temperature--;
+  };
 }
