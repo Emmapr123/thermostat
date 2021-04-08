cb79d69923c43ac9738646eb507aba1865ee4ce0
Natalia_Sherchenkova <tataterer@gmail.com>
2021-04-08 16:45:30 +0100

    Outdoor weather feature added with an API key

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ powersavingmode status
 powersavingmode button
 
 energy usage
+
+
+To call the weather API
+
+$.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=289b88a2e3e5fb781d9de6163f5e3c39', function(data) {
+  console.log(data.main.temp);
+})
+
+
+
+$.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=289b88a2e3e5fb781d9de6163f5e3c39&units=metric', function(data) {
+  console.log(data.main.temp);
+})
--- interface.js
+++ interface.js
@@ -3,16 +3,21 @@ let thermostat = new Thermostat;
 $(document).ready(function() {
   $('#temperature').text(thermostat.temperature)
   $('#powerSavingMode').text(thermostat.powerSavingMode ? 'ON' : 'OFF')
+  $('#energyUsage').text(thermostat.energyUsage());
 });
 
 $('#up').click(function() {
   thermostat.up()
   $('#temperature').text(thermostat.temperature)
+  $('#energyUsage').text(thermostat.energyUsage());
+  $('#energyUsage').attr('class', thermostat.energyUsage());
 });
 
 $('#down').click(function() {
   thermostat.down()
   $('#temperature').text(thermostat.temperature)
+  $('#energyUsage').text(thermostat.energyUsage());
+  $('#energyUsage').attr('class', thermostat.energyUsage());
 });
 
 $('#reset').click(function() {
@@ -24,6 +29,10 @@ $('#powerSavingModeSwitch').click(function() {
   thermostat.powerSavingModeSwitch()
   $('#powerSavingMode').text(thermostat.powerSavingMode ? 'ON' : 'OFF')
 });
+
+$.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=289b88a2e3e5fb781d9de6163f5e3c39&units=metric', function(data) {
+  $('#outdoorTemperature').text(data.main.temp);
+})
 
 
 
